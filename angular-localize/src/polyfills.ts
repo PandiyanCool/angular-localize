70660f69215d9198505d6e65c585e4fa08276498
Pandiyan <pandiyanit7@gmail.com>
2020-01-24 16:50:54 +0530

    Added locales

--- angular-localize/src/polyfills.ts
+++ angular-localize/src/polyfills.ts
@@ -2,6 +2,8 @@
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
 import '@angular/localize/init';
+import { loadTranslations } from '@angular/localize';
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -65,3 +67,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// loadTranslations({
+//     '8298328021206315701': 'Cool'
+//   });
+  
